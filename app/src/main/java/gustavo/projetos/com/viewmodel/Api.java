790ff8479f8984748ad62ee71ef55404fdf616e8
GustavodeJesus <gustavdejesusgomes@hotmail.com>
2018-09-02 18:04:30 -0300

    [COMMIT-MASTER] Projeto MVVM Api Heroes Marvel

---
+++ app/src/main/java/gustavo/projetos/com/viewmodel/Api.java
@@ -0,0 +1,13 @@
+package gustavo.projetos.com.viewmodel;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface Api {
+    String BASE_URL = "https://simplifiedcoding.net/demos/";
+
+    @GET("marvel")
+    Call<List<Hero>> getHeroes();
+}
